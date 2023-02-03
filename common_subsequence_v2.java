10314edadd599bc5d8f0368a3e612f607ba496f8
Ryan Truong <114968495+ryanctruong@users.noreply.github.com>
2023-02-02 18:03:32 -0600

    Update and rename common_subsequence.java to common_subsequence_v2.java

--- common_subsequence_v2.java
+++ common_subsequence_v2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class common_subsequence{
+public class common_subsequence_v2{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
