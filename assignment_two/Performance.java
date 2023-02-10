e175dc76202cb749319a5a94a0ab5d663dd9a415
Ryan Truong <114968495+ryanctruong@users.noreply.github.com>
2023-02-09 23:41:15 -0600

    Update Performance.java

--- assignment_two/Performance.java
+++ assignment_two/Performance.java
@@ -1,18 +1,30 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Performance {
     public static void main(String[] args){
         Tester t;
 
         sorting_algorithms[] sa = {new bubble_sort(), new insertion_sort(), new selection_sort(),
                 new shell_sort(), new quick_sort(), new merge_sort()};
+
+        String[] names = {"Bubble Sort", "Insertion Sort", "Selection Sort", "Shell Sort", "Quick Sort", "Merge Sort"};
+
         int[] s = {100, 500, 1000, 2000, 5000, 10000, 20000, 75000, 150000};
 
-        for(sorting_algorithms algorithms : sa){
-            t = new Tester(algorithms);
-            System.out.println("-------------------------");
-            for(int size : s ){
-                System.out.print("Sorted " + size + " elements in ");
-                t.test(20, size);
+        try{
+            FileWriter fw = new FileWriter("output.txt");
+
+            for(int i = 0; i < sa.length; i++){
+                t = new Tester(sa[i]);
+                fw.write(names[i] + "\n");
+                for(int size : s ){
+                    fw.write("\tSorted " + size + " elements in " + t.test(20, size) + "\n");
+                }
             }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
